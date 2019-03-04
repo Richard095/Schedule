be9761f4bda01544a846b4385a221640cf14c16c
Richard095 <ricardorm789@gmail.com>
2019-03-04 16:39:07 -0600

    Schedule optimized

---
+++ app/src/main/java/com/example/schedule/SchuduleAdapter.java
@@ -0,0 +1,73 @@
+package com.example.schedule;
+
+
+import android.content.Context;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class SchuduleAdapter extends RecyclerView.Adapter<SchuduleAdapter.ViewHolder> {
+
+    private Context context;
+    private ArrayList<Subject>subjectList;
+    public SchuduleAdapter (Context context, ArrayList<Subject> subjectList){
+        this.context = context;
+        this.subjectList = subjectList;
+    }
+
+    @Override
+    public SchuduleAdapter.ViewHolder onCreateViewHolder( ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.subject_list_layout,parent,false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder( SchuduleAdapter.ViewHolder viewHolder, int position) {
+
+        Subject subject = subjectList.get(position);
+
+        viewHolder.tv_subject_name.setText(subject.getSubject_name());
+        viewHolder.tv_teacher.setText(subject.getTeacher());
+        viewHolder.tv_classroom.setText(subject.getClassroom());
+        viewHolder.tv_start_class.setText(Integer.toString(subject.getHour_start_class())+":00");
+        viewHolder.tv_end_class.setText(Integer.toString(subject.getHour_end_class())+":00");
+        viewHolder.tv_indicator.setText(subject.getIndicator());
+        viewHolder.iv_On_Classe.setImageResource(subject.getImage_on_off());
+        //viewHolder.iv_On_Classe.setImageDrawable(ContextCompat.getDrawable(context,subject.getImage_on_off()));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return subjectList.size();
+    }
+
+     class ViewHolder extends  RecyclerView.ViewHolder{
+
+        public TextView tv_subject_name;
+        public TextView tv_teacher;
+        public TextView tv_classroom;
+        public TextView tv_start_class;
+        public TextView tv_end_class;
+        public TextView tv_indicator;
+        public ImageView iv_On_Classe;
+
+        public ViewHolder( View itemView) {
+            super(itemView);
+            tv_subject_name = itemView.findViewById(R.id.tv_subject_name);
+            tv_teacher = itemView.findViewById(R.id.tv_teacher);
+            tv_classroom = itemView.findViewById(R.id.tv_classroom);
+            tv_start_class = itemView.findViewById(R.id.tv_start_class);
+            tv_end_class = itemView.findViewById(R.id.tv_end_class);
+            tv_indicator = itemView.findViewById(R.id.tv_indicator);
+            iv_On_Classe =itemView.findViewById(R.id.iv_On_Classe);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/schedule/Subject.java
@@ -0,0 +1,88 @@
+package com.example.schedule;
+
+public class Subject {
+    private int day;
+    private String subject_name;
+    private String teacher;
+    private String classroom;
+    private int hour_start_class;
+    private int hour_end_class;
+    private String indicator;  //AM | PM
+    private int image_on_off;
+
+    public Subject(int day,String subject_name,String teacher,String classroom,int hour_start_class,
+                   int hour_end_class,String indicator,int image_on_off){
+        this.day = day;
+        this.subject_name=subject_name;
+        this.teacher = teacher;
+        this.classroom = classroom;
+        this.hour_start_class=hour_start_class;
+        this.hour_end_class = hour_end_class;
+        this.indicator = indicator;
+        this.image_on_off = image_on_off;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public String getSubject_name() {
+        return subject_name;
+    }
+
+    public void setSubject_name(String subject_name) {
+        this.subject_name = subject_name;
+    }
+
+    public String getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(String teacher) {
+        this.teacher = teacher;
+    }
+
+    public String getClassroom() {
+        return classroom;
+    }
+
+    public void setClassroom(String classroom) {
+        this.classroom = classroom;
+    }
+
+    public int getHour_start_class() {
+        return hour_start_class;
+    }
+
+    public void setHour_start_class(int hour_start_class) {
+        this.hour_start_class = hour_start_class;
+    }
+
+    public int getHour_end_class() {
+        return hour_end_class;
+    }
+
+    public void setHour_end_class(int hour_end_class) {
+        this.hour_end_class = hour_end_class;
+    }
+
+    public String getIndicator() {
+        return indicator;
+    }
+
+    public void setIndicator(String indicator) {
+        this.indicator = indicator;
+    }
+
+    public int getImage_on_off() {
+        return image_on_off;
+    }
+
+    public void setImage_on_off(int image_on_off) {
+        this.image_on_off = image_on_off;
+    }
+}
