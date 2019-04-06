9799ff0f213c51b4e477c5c1bcd8fd78ee5943bc
Richard095 <ricardorm789@gmail.com>
2019-04-06 16:48:14 -0600

    UPDATED

--- app/src/main/java/com/example/schedule/ScheduleFragment.java
+++ app/src/main/java/com/example/schedule/ScheduleFragment.java
@@ -154,16 +154,7 @@ public class ScheduleFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
+    /** Interface */
     public interface OnFragmentInteractionListener {
         void onSubjectSelected(Subject subject,ArrayList<Subject> subjects);
     }
